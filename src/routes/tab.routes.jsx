93895800c99e2a667c3038d08f3e9df41c6a4a48
Pablo Daniel Couto <pablocouto@aluno.senai.br>
2024-03-27 15:20:04 -0300

    feat: edicoes do app

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -7,52 +7,53 @@ import Profile from "../screens/Profile";
 import Filiais from "../screens/Filiais";
 import Details from "../screens/Details";
 import Form from "../screens/Form";
+import User from "../models/user/User";
+import usersRepository from "../models/user/UserRepository";
 
 const Tab = createBottomTabNavigator();
+
+const users = usersRepository.getAll();
 
 const TabRoutes = () => {
   return (
 
     <Tab.Navigator screenOptions={{
-        headerShown: false,
-        tabBarStyle: {backgroundColor: '#121212'}
-      }}>
-        <Tab.Screen
-          name="Filiais"
-          component={Filiais}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Feather
-                name="list"
-                size={24}
-                color={focused ? "#7c5295" : "#fff"}
-              />
-            ),
-            tabBarLabel: "Filiais",
-            tabBarActiveTintColor: "#7c5295",
-            tabBarInactiveTintColor: "#fff",
-          }}
-        />
-        <Tab.Screen
-          name="Form"
-          component={Form}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Feather
-                name="printer"
-                size={24}
-                color={focused ? "#7c5295" : "#fff"}
-              />
-            ),
-            tabBarLabel: "Form",
-            tabBarActiveTintColor: "#7c5295",
-            tabBarInactiveTintColor: "#fff",
-          }}
-        />
-        <Tab.Screen
-          name="Details"
-          component={Details}
-        />
+      headerShown: false,
+      tabBarStyle: { backgroundColor: '#121212' }
+    }}>
+      <Tab.Screen
+        name="Filiais"
+        component={Filiais}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="list"
+              size={24}
+              color={focused ? "#7c5295" : "#fff"}
+            />
+          ),
+          tabBarLabel: "Filiais",
+          tabBarActiveTintColor: "#7c5295",
+          tabBarInactiveTintColor: "#fff",
+        }}
+      />
+      <Tab.Screen
+        name="Form"
+        component={Form}
+        initialParams={{ user: null, edit: false }}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="printer"
+              size={24}
+              color={focused ? "#7c5295" : "#fff"}
+            />
+          ),
+          tabBarLabel: "Form",
+          tabBarActiveTintColor: "#7c5295",
+          tabBarInactiveTintColor: "#fff",
+        }}
+      />
       <Tab.Screen
         name="Home"
         component={Home}
@@ -60,6 +61,23 @@ const TabRoutes = () => {
           tabBarIcon: ({ focused }) => (
             <Feather
               name="home"
+              size={24}
+              color={focused ? "#7c5295" : "#fff"}
+            />
+          ),
+          tabBarLabel: "Inicial",
+          tabBarActiveTintColor: "#7c5295",
+          tabBarInactiveTintColor: "#fff",
+        }}
+      />
+      <Tab.Screen
+        name="Details"
+        component={Details}
+        initialParams={{ users }}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="file-text"
               size={24}
               color={focused ? "#7c5295" : "#fff"}
             />
